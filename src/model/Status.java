d2fe0d94456d3a00ec701413fed1b22c0c7b9f28
Daria Leo <dashunkaleo@yandex.ru>
2025-06-18 10:22:49 +0300

    исправлены ошибки тестирования CodeStyle в status

--- src/model/Status.java
+++ src/model/Status.java
@@ -1,18 +1,19 @@
 package model;
 
 public enum Status {
-    NEW,
-    IN_PROGRESS,
-    DONE;
+    NEW, IN_PROGRESS, DONE;
 
-    public static Status toStatus(String s){
+    public static Status toStatus(String s) {
         Status status = null;
-        switch (s){
-            case "NEW": status = Status.NEW;
-            break;
-            case "IN_PROGRESS": status = Status.IN_PROGRESS;
-            break;
-            case "DONE": status = Status.DONE;
+        switch (s) {
+            case "NEW":
+                status = Status.NEW;
+                break;
+            case "IN_PROGRESS":
+                status = Status.IN_PROGRESS;
+                break;
+            case "DONE":
+                status = Status.DONE;
         }
         return status;
     }
