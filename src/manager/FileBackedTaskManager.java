477c8895e4eb266c182242512bd7354e774b486c
Daria Leo <dashunkaleo@yandex.ru>
2025-06-18 10:25:08 +0300

    исправлены ошибки тестирования CodeStyle

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -11,36 +11,38 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileBackedTaskManager extends  InMemoryTaskManager{
+public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private File file;
+
     public FileBackedTaskManager(File file) {
         this.file = file;
     }
+
     public void save() throws ManagerSaveException {// сохраняет текущее состояние менеджера в указанный файл
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8))){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, StandardCharsets.UTF_8))) {
             List<Task> totalList = new ArrayList<>(this.getTasks());
             totalList.addAll(this.getEpics());
             totalList.addAll(this.getSubtasks());
-            for(Task task : totalList){
+            for (Task task : totalList) {
                 writer.write(toString(task));
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             throw new ManagerSaveException("Save exception");
             //Исключения вида IOException нужно отлавливать внутри метода save и выкидывать собственное непроверяемое исключение ManagerSaveException.
         }
     }
 
-    public static FileBackedTaskManager loadFromFile(File file){ //восстановление данных из файла
+    public static FileBackedTaskManager loadFromFile(File file) { //восстановление данных из файла
         FileBackedTaskManager fbtm = null;
-        try(BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))){
+        try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
             fbtm = new FileBackedTaskManager(file);
             String str = reader.readLine();
-            while(str != null){
+            while (str != null) {
                 Task task = fromString(str);
-                if(task instanceof Subtask){
+                if (task instanceof Subtask) {
                     fbtm.createSubtask((Subtask) task);
-                } else if (task instanceof Epic){
+                } else if (task instanceof Epic) {
                     fbtm.createEpic((Epic) task);
                 } else {
                     fbtm.createTask(task);
@@ -130,36 +132,38 @@ public class FileBackedTaskManager extends  InMemoryTaskManager{
 1,TASK,Task1,NEW,Description task1,
 2,EPIC,Epic2,DONE,Description epic2,
 3,SUBTASK,Sub Task2,DONE,Description sub task3,2 */
-    private String toString(Task task){
+    private String toString(Task task) {
         int epicId = -1;
         TaskTypes type = null;
-        if(task instanceof Subtask){
+        if (task instanceof Subtask) {
             type = TaskTypes.SUBTASK;
-            epicId =((Subtask) task).getEpicId();
+            epicId = ((Subtask) task).getEpicId();
         } else if (task instanceof Epic) {
             type = TaskTypes.EPIC;
-        } else{
+        } else {
             type = TaskTypes.TASK;
         }
-        String res = String.format("%d, %s, %s, %s, %s, %s%n",
-                task.getId(), type, task.getTitle(), task.getStatus(), task.getDescription(), epicId==-1?"":epicId);
+        String res = String.format("%d, %s, %s, %s, %s, %s%n", task.getId(), type, task.getTitle(), task.getStatus(), task.getDescription(), epicId == -1 ? "" : epicId);
         return res;
     }
 
-    private static Task fromString(String taskStr){
+    private static Task fromString(String taskStr) {
         String[] taskArray = taskStr.split(", ");
-        switch (TaskTypes.toType(taskArray[1])){
-            case TASK: Task task = new Task(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]));
-            task.setId(Integer.parseInt(taskArray[0]));
-            return task;
-            case EPIC: Epic epic = new Epic(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]));
+        switch (TaskTypes.toType(taskArray[1])) {
+            case TASK:
+                Task task = new Task(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]));
+                task.setId(Integer.parseInt(taskArray[0]));
+                return task;
+            case EPIC:
+                Epic epic = new Epic(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]));
                 epic.setId(Integer.parseInt(taskArray[0]));
                 return epic;
-            case SUBTASK: Subtask subtask = new Subtask(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]),
-                    Integer.parseInt(taskArray[5]));
+            case SUBTASK:
+                Subtask subtask = new Subtask(taskArray[2], taskArray[4], Status.toStatus(taskArray[3]), Integer.parseInt(taskArray[5]));
                 subtask.setId(Integer.parseInt(taskArray[0]));
                 return subtask;
-            default: return null;
+            default:
+                return null;
         }
     }
 }
--- src/manager/TaskTypes.java
+++ src/manager/TaskTypes.java
@@ -5,14 +5,17 @@ import model.Status;
 public enum TaskTypes {
     TASK, EPIC, SUBTASK;
 
-    public static TaskTypes toType(String s){
+    public static TaskTypes toType(String s) {
         TaskTypes type = null;
-        switch (s){
-            case "TASK": type = TaskTypes.TASK;
+        switch (s) {
+            case "TASK":
+                type = TaskTypes.TASK;
                 break;
-            case "EPIC": type = TaskTypes.EPIC;
+            case "EPIC":
+                type = TaskTypes.EPIC;
                 break;
-            case "SUBTASK": type = TaskTypes.SUBTASK;
+            case "SUBTASK":
+                type = TaskTypes.SUBTASK;
         }
         return type;
     }
